797966d37f754834430a51f1e29d395d1e0ad909
ShihabShahib <shahibshihab@gmail.com>
2021-09-11 19:37:29 +0600

    BasicCRUDonly

---
+++ foodMenuCRUD/Controllers/MenuController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace foodMenuCRUD.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        fooddbEntities db = new fooddbEntities();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            IEnumerable<menuDetail> listMenu = db.menuDetails;
+            return View(listMenu);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(menuDetail model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.menuDetails.Add(model);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Invalid Operation");
+                    return View();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid Data");
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            menuDetail model = db.menuDetails.Find(id);
+            return View("Create",model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(menuDetail model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry<menuDetail>(model).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Invalid Operation");
+                    return View();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid Data");
+                return View();
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            menuDetail model = db.menuDetails.Find(id);
+            if(model != null)
+            {
+                db.menuDetails.Remove(model);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ foodMenuCRUD/Controllers/XFoodsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace foodMenuCRUD.Controllers
+{
+    public class XFoodsController : Controller
+    {
+        // GET: XFoods
+        [HttpGet]
+        public ActionResult loginAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult loginAdmin(FormCollection values)
+        {
+            if (values["username"] == "admin" && values["password"] == "12345678")
+            {
+                return RedirectToAction("Index", "Menu");
+            }
+            else
+            {
+                ViewBag.msg = "Invalid";
+                return View();
+            }
+        }
+    }
+}
